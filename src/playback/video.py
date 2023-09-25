9c0ebf8465073672e8755fd2f47679a473744b1d
Bludoo <bludoob@gmail.com>
2023-09-25 13:32:35 -0500

    video.py: Begin work on video player

---
+++ src/playback/video.py
@@ -0,0 +1,21 @@
+from tkinter import Frame, Tk
+from tkVideoPlayer import TkinterVideo
+
+class VideoPlayer(Frame):
+    def __init__(self, master, video_path, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.video_path = video_path
+
+        self.tk_video = TkinterVideo(master=self, scaled=True)
+        self.tk_video.load(r"{}".format(self.video_path))
+        self.tk_video.pack(expand=True, fill="both")
+        self.tk_video.set_size((640, 480))
+
+    def play(self):
+        self.tk_video.play()
+
+    def pause(self):
+        self.tk_video.pause()
+
+    def seek(self, seconds):
+        self.tk_video.seek(seconds * 1000)  # converting seconds to milliseconds
