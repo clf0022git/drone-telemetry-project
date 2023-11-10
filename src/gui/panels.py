41189be1b08414e9ab0dff80aa8b7fc2de83b284
Inkaliber <calebwalker1011@gmail.com>
2023-11-09 23:00:33 -0600

    Created save function. Edited swap_metric to swap Celsius to Fahrenheit and vice versa.

--- src/gui/panels.py
+++ src/gui/panels.py
@@ -7,6 +7,7 @@ from tkinter import ttk, filedialog
 from src.playback.video import VideoPlayer
 from src.data.input import DataManager
 from src.data.statistics import DataProcessor
+from src.data.fileManager import FileManager
 
 m_or_f = 0  # 0 = f and 1 = m
 
@@ -23,11 +24,13 @@ class ConfigurationPanel(ttk.Frame):
         # Instantiate a DataManager object
         self.data_manager = DataManager()
         self.data_processor = DataProcessor()
+        self.statistics_list = []
 
         self.label = ttk.Label(self, font=("Roboto Black", 14), text="Configuration Panel")
         self.label.pack(pady=10)
         self.playback_panel = playback_panel
         self.stats_panel = stats_panel
+        self.file_manager = FileManager
 
         # Styles for tkinter widgets
         button_style = ttk.Style()
@@ -49,7 +52,7 @@ class ConfigurationPanel(ttk.Frame):
         self.metricButton = ttk.Button(self.metric_group, text="Swap between meters and feet", command=self.swap_metric)
         self.metricButton.pack(pady=5, side=tk.LEFT)
 
-        self.metric_label = ttk.Label(self.metric_group, font=("Roboto Light", 10), text="Current Metric: Meters")
+        self.metric_label = ttk.Label(self.metric_group, font=("Roboto Light", 10), text="Current Metrics: Meters and Celsius")
         self.metric_label.pack(pady=5, side=tk.RIGHT)
 
         self.statisticsButton = ttk.Button(self, text="Get Statistics", command=self.get_statistics)
@@ -158,12 +161,17 @@ class ConfigurationPanel(ttk.Frame):
 
     def swap_metric(self):
         global m_or_f
+
+        if self.data_manager.data_file.empty:
+            print("Please load data before use")
+            return
+
         self.data_manager.swap_metric()
         if m_or_f == 0:
-            self.metric_label.config(text="Current Metric: Feet")
+            self.metric_label.config(text="Current Metrics: Feet and Fahrenheit")
             m_or_f = 1
         else:
-            self.metric_label.config(text="Current Metric: Meters")
+            self.metric_label.config(text="Current Metrics: Meters and Celsius")
             m_or_f = 0
 
     def get_statistics(self):
@@ -172,19 +180,15 @@ class ConfigurationPanel(ttk.Frame):
             print("No fields selected")
             return None
 
-        file = "C:/Users/caleb/Documents/CS_499/Output_Data/Statistics.txt"
-
-        output_file = open(file, "w")
-        output_file.write("")
-
-        output_file.close()
-
         for element in self.data_manager.user_selected_gauges_list:
             field = element.field_name[0]
             print(element.field_name[0])
             if self.data_manager.data_file[field].dtype == "int64" or self.data_manager.data_file[
                 field].dtype == "float64":
-                self.data_processor.calc_statistics(self.data_manager.data_file[field], field, file)
+                stats = self.data_processor.calc_statistics(self.data_manager.data_file[field], field)
+                print(type(self.statistics_list))
+                self.statistics_list.append(stats)
+        self.file_manager.save_gauges(self.data_manager.user_selected_gauges_list, self.statistics_list)
 
     def load_video(self):
         """Prompt the user to select a video file."""
