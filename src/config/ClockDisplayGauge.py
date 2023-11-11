d3c67c7c6905e2cc21179e125eb365f3d6c42757
Bludoo <12192277+Bludoo@users.noreply.github.com>
2023-11-11 14:02:19 -0600

    Update clock display gauge

--- src/config/ClockDisplayGauge.py
+++ src/config/ClockDisplayGauge.py
@@ -14,6 +14,10 @@ class ClockGauge(GaugeBase):
         self.time_label = tk.Label(self, textvariable=self.time_var, font=('Arial', 24))
         self.time_label.pack(expand=True)
 
+        # Cases for stopwatch mode
+        self.running = False
+        self.elapsed_time_at_pause = timedelta(0)
+
     def get_initial_time_display(self):
         if self.mode == 'clock':
             return datetime.now().strftime("%H:%M:%S")
@@ -26,16 +30,18 @@ class ClockGauge(GaugeBase):
             now = datetime.now()
             self.time_var.set(now.strftime("%H:%M:%S"))
         elif self.mode == 'stopwatch':
-            # If the stopwatch is just starting
-            if self.start_time is None:
-                self.start_time = datetime.now()
-            # Calculate elapsed time
-            elapsed_time = datetime.now() - self.start_time
-            # Format as HH:MM:SS.sss (milliseconds)
-            hours, remainder = divmod(elapsed_time.seconds, 3600)
-            minutes, seconds = divmod(remainder, 60)
-            milliseconds = elapsed_time.microseconds // 1000
-            self.time_var.set(f'{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}')
+            if self.running:
+                # If the stopwatch is just starting
+                if self.start_time is None:
+                    self.start_time = datetime.now()
+                # Calculate elapsed time
+                elapsed_time = datetime.now() - self.start_time
+                # Format as HH:MM:SS.sss (milliseconds)
+                # We don't have strftime() for timedelta, so we have to do this manually
+                hours, remainder = divmod(elapsed_time.seconds, 3600)
+                minutes, seconds = divmod(remainder, 60)
+                milliseconds = elapsed_time.microseconds // 1000
+                self.time_var.set(f'{hours:02d}:{minutes:02d}:{seconds:02d}.{milliseconds:03d}')
         elif self.mode == 'running_time':
             # Display the running time (value should be the number of seconds elapsed)
             if value is not None:
@@ -46,6 +52,28 @@ class ClockGauge(GaugeBase):
 
     def reset_stopwatch(self):
         self.start_time = None
+
+    def toggle_stopwatch(self):
+        """Toggle the stopwatch start/pause."""
+        if self.running:
+            self.pause_stopwatch()
+        else:
+            self.start_stopwatch()
+
+    def start_stopwatch(self):
+        """Start the stopwatch."""
+        if not self.running:
+            self.running = True
+            if self.start_time is None:
+                self.start_time = datetime.now() - self.elapsed_time_at_pause
+            self.update_value()
+
+    def pause_stopwatch(self):
+        """Pause the stopwatch."""
+        if self.running:
+            self.running = False
+            self.elapsed_time_at_pause = datetime.now() - self.start_time
+            self.start_time = None
 
     def set_mode(self, mode):
         self.mode = mode
@@ -66,22 +94,27 @@ if __name__ == "__main__":
     stopwatch_gauge = ClockGauge(root, title='Stopwatch', description='Elapsed Time', mode='stopwatch')
     stopwatch_gauge.pack(padx=10, pady=10)
 
+    # Button to toggle the stopwatch start/pause
+    toggle_button = tk.Button(root, text="Start/Stop", command=stopwatch_gauge.toggle_stopwatch)
+    toggle_button.pack(pady=5)
+
     # Create a ClockGauge instance for the running time
     running_time_gauge = ClockGauge(root, title='Running Time', description='Video Time', mode='running_time')
     running_time_gauge.pack(padx=10, pady=10)
 
-    video_time_seconds = 0  # This is now a global variable
+    video_time_seconds = 0
 
     def update_clock():
         clock_gauge.update_value()
         root.after(1000, update_clock)  # Update the clock every second
 
     def update_stopwatch():
-        stopwatch_gauge.update_value()
-        root.after(100, update_stopwatch)  # Update the stopwatch every second
+        if stopwatch_gauge.running:
+            stopwatch_gauge.update_value()
+        root.after(100, update_stopwatch)  # Update the stopwatch every 100 milliseconds
 
     def update_running_time():
-        global video_time_seconds  # Declare the use of the global variable
+        global video_time_seconds
         running_time_gauge.update_value(video_time_seconds)
         video_time_seconds += 1
         root.after(1000, update_running_time)  # Increment the running time every second
