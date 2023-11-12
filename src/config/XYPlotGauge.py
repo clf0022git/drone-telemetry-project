f4dd6a2d2e55239139b06b6937823c57e5114344
Bludoo <12192277+Bludoo@users.noreply.github.com>
2023-11-11 18:43:50 -0600

    Add X-by-Y plot gauge

--- src/config/XYPlotGauge.py
+++ src/config/XYPlotGauge.py
@@ -1,168 +1,106 @@
+import tkinter as tk
 from src.config.GaugeBase import GaugeBase
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-#import numpy as np
-
-
-# class XYPlotGauge(GaugeBase):
-#     def __init__(self, master, name='Default', x_range=(0, 100), y_range=(0, 100), **kwargs):
-#         super().__init__(master, name, **kwargs)
-#         self.x_range = x_range
-#         self.y_range = y_range
-#         self.figure = Figure(figsize=(5, 5), dpi=100)
-#         self.plot = self.figure.add_subplot(111)
-#         self.plot.set_xlim(self.x_range)
-#         self.plot.set_ylim(self.y_range)
-#
-#         # Embed the matplotlib figure in the tkinter canvas
-#         self.canvas = FigureCanvasTkAgg(self.figure, master)
-#         self.canvas_widget = self.canvas.get_tk_widget()
-#         self.canvas_widget.pack()
-#
-#         self.points, = self.plot.plot([], [], 'o')
-#
-#     def update_value(self, xy_values):
-#         x, y = xy_values
-#         self.points.set_data(x, y)
-#         self.check_alarm(max(x, y))  # Check for alarm conditions
-#
-#         # Update the plot with the new point
-#         self.canvas.draw()
-#
-#     def check_alarm(self, value):
-#         super().check_alarm(value)
-#         color = 'red' if value > self.red_limit else 'blue'
-#         self.points.set_color(color)
-
-# class XYPlotGauge(GaugeBase):
-#     def __init__(self, master, name='Default', x_range=(0, 100), y_range=(0, 100), **kwargs):
-#         super().__init__(master, name, **kwargs)
-#         self.x_range = x_range
-#         self.y_range = y_range
-#         self.figure = Figure(figsize=(5, 5), dpi=100)
-#         self.plot = self.figure.add_subplot(111)
-#         self.plot.set_xlim(self.x_range)
-#         self.plot.set_ylim(self.y_range)
-#
-#         # Embed the matplotlib figure in the tkinter canvas
-#         self.canvas = FigureCanvasTkAgg(self.figure, master)
-#         self.canvas_widget = self.canvas.get_tk_widget()
-#         self.canvas_widget.pack()
-#
-#         # Initialize empty lists to store the x and y values
-#         self.x_values = []
-#         self.y_values = []
-#
-#         # Plot an empty line, we will add points to this line in the update_value method
-#         self.points, = self.plot.plot([], [], 'o')
-#
-#     def update_value(self, xy_values):
-#         x, y = xy_values
-#         # Append the new x and y values to the lists
-#         self.x_values.append(x)
-#         self.y_values.append(y)
-#
-#         # Set the new data for the points
-#         self.points.set_data(self.x_values, self.y_values)
-#
-#         # Check for alarm conditions
-#         if max(x, y) > self.red_limit:
-#             self.check_alarm(max(x, y))
-#             self.points.set_color('red')  # Set the point's color to red if the alarm condition is met
-#         else:
-#             self.points.set_color('blue')  # Set the point's color to blue otherwise
-#
-#         # Re-draw the canvas to show the updated plot
-#         self.canvas.draw()
-
-
-# if __name__ == "__main__":
-#     import tkinter as tk
-#     import numpy as np
-#     root = tk.Tk()
-#     xy_plot_gauge = XYPlotGauge(root, name="XY Plot Gauge", x_range=(0, 100), y_range=(0, 100))
-#
-#
-#     # Function to update the plot over time
-#     def update_plot():
-#         # Generate some random x, y values for demonstration
-#         x, y = np.random.rand(2) * 100
-#         xy_plot_gauge.update_value((x, y))
-#         root.after(1000, update_plot)
-#
-#
-#     # Start updating the plot
-#     update_plot()
-#
-#     root.mainloop()
-
-import matplotlib.pyplot as plt
 
 
 class XYPlotGauge(GaugeBase):
-    def __init__(self, master, name='Default', x_range=(0, 100), y_range=(0, 100), **kwargs):
-        super().__init__(master, name, **kwargs)
-        self.x_range = x_range
-        self.y_range = y_range
-        self.figure = Figure(figsize=(5, 5), dpi=100)
-        self.plot = self.figure.add_subplot(111)
-        self.plot.set_xlim(self.x_range)
-        self.plot.set_ylim(self.y_range)
+    def __init__(self, master, name='XY Plot Gauge', title='XY Plot', description='', connect_dots=False, *args, **kwargs):
+        super().__init__(master, name=name, title=title, description=description, *args, **kwargs)
 
-        # Embed the matplotlib figure in the tkinter canvas
-        self.canvas = FigureCanvasTkAgg(self.figure, master)
+        # Initialize the figure and axis for the plot
+        self.figure = Figure(figsize=(5, 4), dpi=100)
+        self.axis = self.figure.add_subplot(111)
+
+        # Create the canvas and add it to the GaugeBase frame
+        self.canvas = FigureCanvasTkAgg(self.figure, self)
         self.canvas_widget = self.canvas.get_tk_widget()
-        self.canvas_widget.pack()
+        self.canvas_widget.pack(fill=tk.BOTH, expand=True)
 
+        # Initialize lists to hold the x and y values
         self.x_values = []
         self.y_values = []
         self.colors = []
 
-    def update_value(self, xy_values):
-        x, y = xy_values
+        self.connect_dots = connect_dots
+        if self.connect_dots:
+            self.line, = self.axis.plot([], [], color='black')
+
+        # Additional text for the matplotlib figure
+        self.figure_text = None
+
+    def update_value(self, x, y, value):
+        """Update the plot with a new data point and its value."""
+        # Add the new data point and its color based on the value
         self.x_values.append(x)
         self.y_values.append(y)
-
-        # Determine color based on the threshold
-        color = 'red' if max(x, y) > self.red_limit else 'blue'
+        color = self.get_color_for_value(value)
         self.colors.append(color)
 
-        # Check for alarm conditions
-        if max(x, y) > self.red_limit:
-            self.check_alarm(max(x, y))
+        # Plot the new data point
+        self.axis.scatter(x, y, color=color)
+        self.canvas.draw_idle()
 
-        # Clear the plot to draw new points and lines
-        self.plot.clear()
-        self.plot.set_xlim(self.x_range)
-        self.plot.set_ylim(self.y_range)
+        # If connect_dots is True, update the line with the new data point
+        if self.connect_dots:
+            self.line.set_data(self.x_values, self.y_values)
+            self.canvas.draw_idle()
 
-        # Plot the line connecting all points
-        self.plot.plot(self.x_values, self.y_values, marker='', linestyle='-', color='gray')
+        # Check for alarm condition
+        self.check_alarm(value)
 
-        # Plot points with their respective colors
-        for i in range(len(self.x_values)):
-            self.plot.scatter(self.x_values[i], self.y_values[i], color=self.colors[i])
+    def get_color_for_value(self, value):
+        """Determine the color of the data point based on the value."""
+        for color, limit in self.color_ranges.items():
+            if value <= limit:
+                return color
+        return 'red'
 
-        # Re-draw the canvas to show the updated plot
-        self.canvas.draw()
+    def set_plot_title(self, title):
+        """Set the title for the matplotlib plot."""
+        self.axis.set_title(title)
+        self.canvas.draw_idle()
+
+    def set_figure_text(self, text):
+        """Set additional text on the matplotlib figure."""
+        if self.figure_text is not None:
+            self.figure_text.remove()
+        # Position the text at the top center of the figure
+        self.figure_text = self.axis.text(0.5, 0.9, text, ha='center', va='center', transform=self.axis.transAxes)
+        self.canvas.draw_idle()
+
+    def set_bounds(self, x_bounds=None, y_bounds=None):
+        """Set the x/y bounds of the plot."""
+        if x_bounds:
+            self.axis.set_xlim(x_bounds)
+        if y_bounds:
+            self.axis.set_ylim(y_bounds)
+        self.canvas.draw_idle()
 
 
 # Example usage
 if __name__ == "__main__":
-    import tkinter as tk
-    import numpy as np
     root = tk.Tk()
-    xy_plot_gauge = XYPlotGauge(root, name="XY Plot Gauge", x_range=(0, 100), y_range=(0, 100))
+    root.title("XY Plot Gauge Example")
 
-    # Function to update the plot over time
-    def update_plot():
-        # Generate some random x, y values for demonstration
-        x, y = np.random.rand(2) * 100
-        xy_plot_gauge.update_value((x, y))
-        root.after(1000, update_plot)
+    xy_plot_gauge = XYPlotGauge(root, title='Drone Position', description='Drone X-Y Plot', connect_dots=True)
+    xy_plot_gauge.pack(fill=tk.BOTH, expand=True)
+
+    xy_plot_gauge.set_bounds(x_bounds=(-10, 10), y_bounds=(-10, 10))
+
+    # Function to simulate data points being added to the plot over time
+    def simulate_data():
+        # Here you would retrieve or generate actual x, y, and value data
+        # For simulation, we're using random data
+        import random
+        new_x, new_y = random.uniform(-10, 10), random.uniform(-10, 10)
+        new_value = random.uniform(0, 100)  # Value used to determine the color
+        xy_plot_gauge.update_value(new_x, new_y, new_value)
+        xy_plot_gauge.set_figure_text(f"Color Value: {new_value}\nX: {new_x}\nY: {new_y}")
+
+        # Schedule the next data point update in 1 second (1000 ms)
+        root.after(1000, simulate_data)
 
 
-    # Start updating the plot
-    update_plot()
-
+    simulate_data()
     root.mainloop()
