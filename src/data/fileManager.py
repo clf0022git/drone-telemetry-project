946d1691e70a965b8135290020a5b66bdf0825e0
Bludoo <12192277+Bludoo@users.noreply.github.com>
2023-11-15 20:07:16 -0600

    Add new file manager

--- src/data/fileManager.py
+++ src/data/fileManager.py
@@ -1,27 +1,49 @@
+from src.config.NumberDisplayGauge import NumberDisplayGauge
+from src.config.XYPlotGauge import XYPlotGauge
+import json
 import os
 
 
 class FileManager:
 
     @staticmethod
-    def save_gauges(gauge_list):
-        curr_dir = os.getcwd()
-        dir_name = os.path.dirname(curr_dir) + r"\assets\savedGauges.txt"
-        outfile = open(dir_name, 'w')
+    def save_gauges(gauge_list, filename="savedGauges.json"):
+        # Prepare the data for JSON serialization
+        gauge_data = []
         for gauge in gauge_list:
-            outfile.write(str(gauge.id) + "\n")
-            outfile.write(gauge.field_name[0] + "\n")
-            outfile.write(gauge.second_field_name + "\n")
-            outfile.write(gauge.gauge_name + "\n")
-            outfile.write(str(gauge.timestamp_value) + "\n")
-            outfile.write(gauge.name + "\n")
-            outfile.write(str(gauge.statistics) + "\n")
-            outfile.write(str(gauge.blue_range_low) + "\n")
-            outfile.write(str(gauge.blue_range_high) + "\n")
-            outfile.write(str(gauge.green_range_low) + "\n")
-            outfile.write(str(gauge.green_range_high) + "\n")
-            outfile.write(str(gauge.yellow_range_low) + "\n")
-            outfile.write(str(gauge.yellow_range_high) + "\n")
-            outfile.write(str(gauge.red_range_low) + "\n")
-            outfile.write(str(gauge.red_range_high) + "\n")
-            outfile.write("\n")
+            data = {
+                'id': gauge.id,
+                'field_name': gauge.field_name[0],
+                'second_field_name': gauge.second_field_name,
+                'gauge_name': gauge.gauge_name,
+                'timestamp_value': gauge.timestamp_value,
+                'name': gauge.name,
+                'statistics': gauge.statistics,
+                'blue_range': (gauge.blue_range_low, gauge.blue_range_high),
+                'green_range': (gauge.green_range_low, gauge.green_range_high),
+                'yellow_range': (gauge.yellow_range_low, gauge.yellow_range_high),
+                'red_range': (gauge.red_range_low, gauge.red_range_high)
+            }
+            gauge_data.append(data)
+
+        # Save the data to a file
+        with open(filename, 'w') as outfile:
+            json.dump(gauge_data, outfile, indent=4)
+
+    @staticmethod
+    def load_gauges(master, data_manager, filename="savedGauges.json"):
+        # Load data from a file
+        with open(filename, 'r') as infile:
+            gauge_data = json.load(infile)
+
+        # Deserialize the JSON data back into gauge objects
+        gauge_list = []
+        for data in gauge_data:
+            # # Create gauge objects here based on the data
+            # if data.gauge_name == 'X-by-Y-plot':
+            #     gauge = XYPlotGauge(master)
+            # elif data.gauge_name == 'Number or Character Display':
+            #     gauge = NumberDisplayGauge(master)
+            #gauge_list.append(gauge)
+
+        return gauge_list
